211bacc9c2459b430c30a9b4e2c6b2de51c2729e
Arindam <bis.ips22@gmail.com>
2023-07-14 13:06:20 +0530

    Product List added

---
+++ src/components/Products/ProductList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './ProductList.css'; // Import the CSS file
+import 'bootstrap/dist/css/bootstrap.min.css';// Import Bootstrap CSS
+
+const ProductList = () => {
+  const productsArr = [
+    {
+      title: 'Colors',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+    },
+    {
+      title: 'Black and white Colors',
+      price: 50,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    },
+    {
+      title: 'Yellow and Black Colors',
+      price: 70,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    },
+    {
+      title: 'Blue Color',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+    }
+  ];
+
+  return (
+    <div className="product-list-container">
+      <h1 className="text-center">Products</h1>
+      <div className="row">
+        {productsArr.map((product, index) => (
+          <div className="col-md-6 mb-4" key={index}>
+            <div className="card product-card" style={{ width: '22rem' }}>
+              <img src={product.imageUrl} alt={product.title} className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">${product.price}</p>
+                <button className="btn btn-primary">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
