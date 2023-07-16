e39aee9c9a70d302a3042da2c822d45ac6d57322
Arindam <bis.ips22@gmail.com>
2023-07-16 13:17:24 +0530

    Home page implemented

--- src/components/External Pages/Home.js
+++ src/components/External Pages/Home.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from '../Layout/Header';
-import { FaPlay } from 'react-icons/fa';
-import './Home.css';
+import React from "react";
+import Header from "../Layout/Header";
+import { FaPlay } from "react-icons/fa";
+import "./Home.css";
 
 const Home = () => {
   return (
@@ -13,6 +13,57 @@ const Home = () => {
         <div className="play-button">
           <FaPlay className="play-icon" />
         </div>
+      </div>
+      <div className="lists-container">
+        <h2 className="lists-heading">TOURS</h2>
+        <ul className="home-list">
+          <li className="list-item">
+            <div className="list-details">
+              <span>JUL 16</span>
+              <span>JUL 19</span>
+              <span>JUL 22</span>
+              <span>JUL 29</span>
+              <span>AUG 2</span>
+              <span>AUG 7</span>
+            </div>
+            <div className="list-details">
+              <span>DETROIT, MI</span>
+              <span>TORONTO, ON</span>
+              <span>BRISTOW, VA</span>
+              <span>PHOENIX, AZ</span>
+              <span>LAS VEGAS, NV</span>
+              <span>CONCORD, CA</span>
+            </div>
+            <div className="list-details">
+              <span>DTE ENERGY MUSIC THEATRE</span>
+              <span>BUDWEISER STAGE</span>
+              <span>JIGGY LUBE LIVE</span>
+              <span>AK-CHIN PAVILION</span>
+              <span>T-MOBILE ARENA</span>
+              <span>CONCORD PAVILION</span>
+            </div>
+            <div className="list-details">
+              <span>
+                <button className="buy-tickets-button">BUY TICKETS</button>
+              </span>
+              <span>
+                <button className="buy-tickets-button">BUY TICKETS</button>
+              </span>
+              <span>
+                <button className="buy-tickets-button">BUY TICKETS</button>
+              </span>
+              <span>
+                <button className="buy-tickets-button">BUY TICKETS</button>
+              </span>
+              <span>
+                <button className="buy-tickets-button">BUY TICKETS</button>
+              </span>
+              <span>
+                <button className="buy-tickets-button">BUY TICKETS</button>
+              </span>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
